49ba3db6a27f101432b07ea78d68a56bdebf91e7
biblecampvictim6969 <stefko.richard@norskakladno.cz>
2025-03-13 16:31:00 +0100

    i cant breathe

--- program.py
+++ program.py
@@ -6,3 +6,8 @@ app = Flask(__name__)
 def hello_world():
     return "<p>once i was a 7 years old i sat on a banana and that changed my life</p>"
 
+@app.route("/national_slave_farm")
+def national_slave_farm():
+    return "<p>pls master leave me alone. GET BACK TO WORK MONKEY</p>"
+
+
