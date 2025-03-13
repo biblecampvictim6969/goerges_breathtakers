75e8c12728924579e4790598398d2b479dc4cce3
biblecampvictim6969 <stefko.richard@norskakladno.cz>
2025-03-13 16:23:38 +0100

    adolf was right

--- program.py
+++ program.py
@@ -4,5 +4,5 @@ app = Flask(__name__)
 
 @app.route("/")
 def hello_world():
-    return "<p>Hello, World!</p>"
+    return "<p>once i was a 7 years old i sat on a banana and that changed my life</p>"
 
